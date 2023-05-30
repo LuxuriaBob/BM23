4b179489cf4bed20a9b1bea74d28af32409c8e73
LuxuriaBob <yalinhosgor@gmail.com>
2023-05-30 17:20:04 +0300

    First Commit
    Main menu and settings page added.

---
+++ BM23/Pages/MainMenu.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace BM23.Pages;
+
+public partial class MainMenu : ContentPage
+{
+	public MainMenu()
+	{
+		InitializeComponent();
+	}
+
+    void NewGameBtn_Clicked(System.Object sender, System.EventArgs e)
+    {
+
+    }
+
+    void LoadGameBtn_Clicked(System.Object sender, System.EventArgs e)
+    {
+        Console.WriteLine("loadgame");
+    }
+
+    void SettingsBtn_Clicked(System.Object sender, System.EventArgs e)
+    {
+        Navigation.PushAsync(new Settings());
+    }
+
+    async void ExitBtn_Clicked(System.Object sender, System.EventArgs e)
+    {
+        bool answer = await DisplayAlert("Exiting the game", "Are you sure you want to exit?", "Yes", "No");
+        if (answer == true)
+        {
+            Application.Current.Quit();
+        }
+    }
+}
---
+++ BM23/Pages/Settings.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BM23.Pages;
+
+public partial class Settings : ContentPage
+{
+	public Settings()
+	{
+		InitializeComponent();
+	}
+}
